d6d8700f0a6d97b07e64cc0292bd3d8a3eeb87c3
Marco E <marco.esposito@gmail.com>
2019-12-08 19:10:57 +0100

    update installation script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ script=~/bin/tnt-is-dead
 cat <<EOF > "$script"
 #!/usr/bin/env bash
 cd ~/workspace/tnt_is_dead
-pipenv run python ./main.py \$*
+pipenv run python ./main.py "\$@"
 EOF
 
 chmod +x "$script"
